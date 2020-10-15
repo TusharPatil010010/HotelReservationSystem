1000440bc7ba66f6b40156b6f68441db863617cb
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-15 23:13:56 +0530

    Added functionality to find the cheapest hotel for a given dat range

--- src/main/java/com/capg/hotelreservation/HotelReservation.java
+++ src/main/java/com/capg/hotelreservation/HotelReservation.java
@@ -1,5 +1,7 @@
 package com.capg.hotelreservation;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,6 +34,64 @@ public class HotelReservation {
 			System.out.println("Hotel Name : " + entry.getKey());
 			System.out.println("Rate on weekdays for regular customers : " + entry.getValue().getRegWeekdayRate());
 		}
+	}
+	
+	/**
+	 * UC2 Find cheapest hotel for the given date range
+	 * 
+	 * @param fromDate
+	 * @param toDate
+	 * @return
+	 */
+	public String findCheapestHotel(String fromDate, String toDate) {
+		Map<Integer, ArrayList<Hotel>> rentMap = createRentMap(fromDate, toDate);
+		int minimumRent = Integer.MAX_VALUE; //Assigns max possible value
+		for (Map.Entry<Integer, ArrayList<Hotel>> entry : rentMap.entrySet()) {
+			if (entry.getKey() < minimumRent)
+				minimumRent = entry.getKey();
+		}
+
+		System.out.println("Cheapest Hotel for you is " + rentMap.get(minimumRent).get(0).getHotelName());
+		System.out.println("Total Rent : " + minimumRent);
+		return rentMap.get(minimumRent).get(0).getHotelName();
+	}
+	
+	/**
+	 * Creates rent map of hotels that are added
+	 * No need to repeat the same code to find the rent, following DRY
+	 * 
+	 * @param fromDate
+	 * @param toDate
+	 * @return
+	 */
+	public static Map<Integer, ArrayList<Hotel>> createRentMap(String fromDate, String toDate) {
+		HashMap<Integer, ArrayList<Hotel>> rentMap = new HashMap<>();
+		int numOfDays = numberOfDays(fromDate, toDate);
+		for (Map.Entry<String, Hotel> entry : hotelMap.entrySet()) {
+			int rent = entry.getValue().getRegWeekdayRate() * numOfDays;
+			rentMap.computeIfAbsent(rent, k -> new ArrayList<>()).add(entry.getValue());
+		}
+		return rentMap;
+	}
+
+	/**
+	 * Calculate no. of week days between two dates
+	 * 
+	 * @param fromDate
+	 * @param toDate
+	 * @return
+	 */
+	public static int numberOfDays(String fromDate, String toDate) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+		
+		LocalDate from = LocalDate.parse(fromDate, formatter);    // convert String to LocalDate
+		LocalDate to = LocalDate.parse(toDate, formatter);		 // convert String to LocalDate
+		int numOfDays = 0;
+
+		for (LocalDate date = from; date.isBefore(to); date = date.plusDays(1)) {
+			numOfDays++;
+		}
+		return numOfDays;
 	}
 
 	public static void main(String[] args) {
--- src/test/java/com/capg/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/capg/hotelreservation/HotelReservationTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 
 class HotelReservationTest {
 
+	/**
+	 * UC1 testing
+	 */
 	@Test
 	void whenNewHotelAdded_shouldReturnTrue() {
 
@@ -14,5 +17,17 @@ class HotelReservationTest {
 		assertTrue(hotelReservationObject.addHotel("Ridgewood", 110));
 
 		hotelReservationObject.printHotels();
+	}
+	
+	/**
+	 * UC2 Testing
+	 */
+	@Test
+	void whenFindCheapestHotelMethodCalled_shouldReturn_nameOfHotel() {
+		HotelReservation hotelReservation = new HotelReservation();
+		assertTrue(hotelReservation.addHotel("Lakewood", 110));
+		assertTrue(hotelReservation.addHotel("Bridgewood", 160));
+		assertTrue(hotelReservation.addHotel("Ridgewood", 220));
+		assertEquals("Lakewood", hotelReservation.findCheapestHotel("10 Sep 2020", "11 Sep 2020"));
 	}
 }
